3eaa57d08424ce204d67b78ce8756ea7f580a781
Dhnnz12 <dhonanhibrizi123@gmail.com>
2025-11-06 13:56:26 +0700

    menambahkan Scaffold,TopAppBar dan Column

--- app/src/main/java/com/example/questnavigasiui_071/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigasiui_071/view/TampilData.kt
@@ -1,7 +1,29 @@
 package com.example.questnavigasiui_071.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.questnavigasiui_071.R
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -9,5 +31,27 @@ import androidx.compose.runtime.Composable
 fun TampilData(
     OnBackBtnClick:()-> Unit
 ){
-
+    val items = listOf(
+        Pair(stringResource(R.string.nama_lengkap),"Contoh Nama"),
+        Pair(stringResource(R.string.jenis_kelamin),"Lainnya"),
+        Pair(stringResource(R.string.alamat),"Yogyakarta")
+    )
+    Scaffold (modifier = Modifier,
+        {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.tampil), color = Color.White) },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(colorResource(id = R.color.teal_700))
+            )
+        }) { isiRuang->
+        Column(modifier = Modifier.padding(isiRuang),
+            verticalArrangement = Arrangement.SpaceBetween) {
+            Column(modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_medium)),
+                verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small))) {
+                items.forEach { item ->
+                    Column {
+                    }
+                }
+            }
+        }
+    }
 }
