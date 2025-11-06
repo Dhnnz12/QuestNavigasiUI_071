541364747d1473f885300956874ffe71aa061ed5
Dhnnz12 <dhonanhibrizi123@gmail.com>
2025-11-06 13:40:00 +0700

    MEnambahkan HorizontalDivider

--- app/src/main/java/com/example/questnavigasiui_071/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasiui_071/view/Formulir.kt
@@ -13,6 +13,8 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.DividerDefaults
+import androidx.compose.material3.DividerDefaults.Thickness
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -70,6 +72,11 @@ fun FormIsian(
                     }
                 }
             }
+            HorizontalDivider(modifier = Modifier
+                .padding(20.dp)
+                .width(250.dp),
+            thickness = 1.dp,
+                color = Color.Red)
 
         }
     }
