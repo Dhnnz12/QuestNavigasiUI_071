8ff58b163082c2f796897cd32f6c8d0240a917b5
Dhnnz12 <dhonanhibrizi123@gmail.com>
2025-11-06 13:14:04 +0700

    MENAmbahkan function Form Isian

---
+++ app/src/main/java/com/example/questnavigasiui_071/view/Formulir.kt
@@ -0,0 +1,19 @@
+@file:OptIn(ExprimentalMaterial3Api::class)
+
+package com.example.questnavigasiui_071.view
+
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun FormIsian(
+    jenisK: List<String> = listOf("Laki-laki","Perempuan"),
+    OnSubmitBtnClick : () -> Unit
+){
+    Scaffold (modifier = Modifier,
+        )
+}
+
+
