309fc10507a81d81d0e47aa6b55eec53cd2c19ba
Dhnnz12 <dhonanhibrizi123@gmail.com>
2025-11-06 13:18:15 +0700

    Menambahkan TopAppBar dan Scaffold

--- app/src/main/java/com/example/questnavigasiui_071/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasiui_071/view/Formulir.kt
@@ -3,8 +3,12 @@
 package com.example.questnavigasiui_071.view
 
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
 
 
 @Composable
@@ -12,7 +16,15 @@ fun FormIsian(
     jenisK: List<String> = listOf("Laki-laki","Perempuan"),
     OnSubmitBtnClick : () -> Unit
 ){
-    Scaffold (modifier = Modifier,
+    Scaffold (modifier =Modifier,
+        {
+            TopAppBar(
+                title = {Text(stringResource(R.string.home),
+                    color = Color.White)},
+                colors = TopAppBarDefaults.topAppBarColors
+                    (colorResource(R.color.teal_700))
+            )}
+        }
         )
 }
 
