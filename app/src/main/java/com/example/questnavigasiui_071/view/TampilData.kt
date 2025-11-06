7956aa2af805b5146a6e4398e91c8613cb4d6803
Dhnnz12 <dhonanhibrizi123@gmail.com>
2025-11-06 14:03:21 +0700

    menambahkan spacer,HorizontalDivider dan Button

--- app/src/main/java/com/example/questnavigasiui_071/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigasiui_071/view/TampilData.kt
@@ -49,9 +49,21 @@ fun TampilData(
                 verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small))) {
                 items.forEach { item ->
                     Column {
+                        Text(text = item.first.uppercase(),
+                            fontSize = 16.sp)
+                        Text(text = item.second,
+                            fontWeight = FontWeight.Bold,
+                            fontFamily = FontFamily.Cursive, fontSize = 22.sp)
+                    }
+                    HorizontalDivider(thickness = 1.dp, color = Color.Cyan)
+                }
+                Spacer(modifier = Modifier.height(height = 10.dp))
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = OnBackBtnClick)  {
+                    Text(text = stringResource(id=R.string.back))
                     }
                 }
             }
         }
     }
-}
