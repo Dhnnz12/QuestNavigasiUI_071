7637f723791fe7ceb54ee40bd22272920ac6fc07
Dhnnz12 <dhonanhibrizi123@gmail.com>
2025-11-06 13:27:02 +0700

    MEnambahkan Column

--- app/src/main/java/com/example/questnavigasiui_071/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasiui_071/view/Formulir.kt
@@ -1,7 +1,11 @@
-@file:OptIn(ExprimentalMaterial3Api::class)
+@file:OptIn(ExperimentalMaterial3Api::class)
 
 package com.example.questnavigasiui_071.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
@@ -9,7 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
-
+import com.example.questnavigasiui_071.R
 
 @Composable
 fun FormIsian(
@@ -23,9 +27,13 @@ fun FormIsian(
                     color = Color.White)},
                 colors = TopAppBarDefaults.topAppBarColors
                     (colorResource(R.color.teal_700))
-            )}
+
+            )
         }
-        )
+        ){ isiRuang ->
+        Column (modifier = Modifier.padding(isiRuang),
+            verticalArrangement = Arrangement.Top,)
+    }
 }
 
 
