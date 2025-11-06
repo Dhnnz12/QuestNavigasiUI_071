49a4a363bc9503c2d861ad0947375b9381c81ac6
Dhnnz12 <dhonanhibrizi123@gmail.com>
2025-11-06 12:15:00 +0700

    Menambahkan Scalffold

--- app/src/main/java/com/example/questnavigasiui_071/Navigasi.kt
+++ app/src/main/java/com/example/questnavigasiui_071/Navigasi.kt
@@ -1,7 +1,10 @@
 package com.example.questnavigasiui_071
 
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import java.lang.reflect.Modifier
 
@@ -17,5 +20,20 @@ fun DataApp(
     navController: NavController = rememberNavController(),
     modifier: Modifier
 ){
+    Scaffold { isiRuang->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulirku.name,
+
+            modifier = Modifier.padding(isiRuang)) {
+            composable(route = Navigasi.Formulirku.name) {
+                FormIsian(
+                    OnsubmitBtnClick = {
+                        navController.navigate(Navigasi.Detail.name)
+                    }
+                )
+            }
+        }
+    }
 
 }
